5a0085d53ad4415e9e6f247669bf2537ea8d2157
Christopher Butler <christopher.harold.butler@gmail.com>
2020-02-13 00:36:13 +0100

    stories list with refresh

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'news_api_provider.dart';
+import 'news_db_provider.dart';
+import '../models/item_model.dart';
+
+
+
+abstract class Source {
+  Future<List<int>> fetchTopIds();
+  Future<ItemModel> fetchItem(int id);
+}
+
+abstract class Cache {
+  Future<int> addItem(ItemModel item);
+  Future<int> clear();
+}
+
+class Repository {
+  // NewsDbProvider dbProvider = NewsDbProvider();
+  // NewsApiProvider apiProvider = NewsApiProvider();
+  List<Source> sources = <Source>[newsDbProvider, NewsApiProvider()];
+  List<Cache> caches = <Cache>[newsDbProvider];
+
+
+  // iterate over sources after implementing dbProvider's fetchTopIds
+  Future<List<int>> fetchTopIds() {
+    return sources[1].fetchTopIds();
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    // var item = await dbProvider.fetchItem(id);
+    // if (item != null) {
+    //   return item;
+    // }
+
+    // item = await apiProvider.fetchItem(id);
+
+    // dbProvider.addItem(item);
+
+    // return item;
+    ItemModel item;
+    Source source;
+
+    for (source in sources) {
+      item = await source.fetchItem(id);
+      if (item != null) break;
+    }
+
+    for (var cache in caches) {
+      if (cache != source) {
+        cache.addItem(item);
+      }
+    }
+
+    return item;
+  }
+
+  clearCache() async {
+    for (var cache in caches) {
+      await cache.clear();
+    }
+  }
+}
+
+
+
+
